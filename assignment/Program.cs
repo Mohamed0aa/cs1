178f31ba1aea057aa2e5ddad7901ec281f8acd18
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-17 18:36:07 +0200

    q1

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -9,29 +9,7 @@
             //Console.WriteLine("number is "+x);
             #endregion
 
-            #region q2 Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
-            // int x = 10;
-            //int y = x;
-            //Console.WriteLine("y = "+y);
-            //Console.WriteLine("x = " + x);
-            //x = 20;
-            //Console.WriteLine("y = " + y);
-            //Console.WriteLine("x = " + x);
-            //x=20  y=10 no change happend on y
-            #endregion
-
-            #region q3 Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
-            // colour car = new colour();
-            // colour car1 = new colour();
-            // car1 = car;
-            // car.name = "red";
-            //Console.WriteLine(" car1 = "+car1.name);
-            //Console.WriteLine(" car1 = " + car.name);
-            // car.name = "green";
-            //  Console.WriteLine(" car1 = " + car1.name);
-            //   Console.WriteLine(" car1 = " + car.name);
-            //modification is affect in both varible
-            #endregion
+           
         }
     }
 }
