1cfc458da553a8ce3fc87a855b20a5a3896aab6c
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-17 18:38:06 +0200

    q3

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -2,6 +2,10 @@
 {
     internal class Program
     {
+        class colour
+        {
+            public string name;
+        }
         static void Main(string[] args)
         {
             #region q1 Write a program that allows the user to enter a number then print it.
@@ -18,6 +22,18 @@
             //Console.WriteLine("y = " + y);
             //Console.WriteLine("x = " + x);
             //x=20  y=10 no change happend on y
+            #endregion
+            #region q3 Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+            // colour car = new colour();
+            // colour car1 = new colour();
+            // car1 = car;
+            // car.name = "red";
+            //Console.WriteLine(" car1 = "+car1.name);
+            //Console.WriteLine(" car1 = " + car.name);
+            // car.name = "green";
+            //  Console.WriteLine(" car1 = " + car1.name);
+            //   Console.WriteLine(" car1 = " + car.name);
+            //modification is affect in both varible
             #endregion
 
 
